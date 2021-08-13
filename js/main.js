5a3333313e2e8e0fa40b1b16be5eafc8891db808
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-08-14 04:58:22 +0500

    home section were reponsive

--- js/main.js
+++ js/main.js
@@ -2,3 +2,6 @@ window.addEventListener('scroll',function(){
     var navScroll=document.getElementById('nav');
     navScroll.classList.toggle('fixed',window.scrollY>10)
 })
+function navBack(){
+    var nav = document.getElementById('nav').classList.toggle('change');
+}
